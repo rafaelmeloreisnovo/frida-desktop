d43d9d78cb96945a256f90be4eb6b87cfe1303b8
Rafael mreis <rafaelmeloreisnovo@gmail.com>
2026-09-07 00:19:28 -0300

    feat(android): codify safe operator defaults

---
+++ android/app/src/io/rafaelia/fridalab/OperatorDefaults.java
@@ -0,0 +1,7 @@
+package io.rafaelia.fridalab;
+
+final class OperatorDefaults {
+    private OperatorDefaults() {}
+    static final int LEARNING_MODE = 0;
+    static final String CLAIM_ALLOWED = "false";
+}
